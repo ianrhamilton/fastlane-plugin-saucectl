93e140d246c8619d1154b67dda623c1cdde11a9a
Ian Hamilton <ian.hamilton@dnata.com>
2021-12-20 22:51:27 +0400

    fix installer spec

--- spec/rsaucectl_installer_spec.rb
+++ spec/rsaucectl_installer_spec.rb
@@ -1,5 +1,6 @@
 require_relative 'spec_helper'
 require_relative 'utils/mock_api'
+require_relative '../lib/fastlane/plugin/rsaucectl/helper/installer'
 
 describe Fastlane::Saucectl::Installer do
   before do
@@ -13,7 +14,7 @@ describe Fastlane::Saucectl::Installer do
   describe 'installer' do
     it 'should download saucectl binary' do
       Saucectl::MockApi.new.download
-      installer = Fastlane::Saucectl::Installer.install
+      installer = Fastlane::Saucectl::Installer.new.install
       expect(installer).to be_truthy
     end
   end
