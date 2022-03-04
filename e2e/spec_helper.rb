28f4588d1f7641c38ab8237d3842236f643e9cb5
Ian Hamilton <1120597+ianrhamilton@users.noreply.github.com>
2022-03-04 11:17:27 +0400

    upadted test app

--- e2e/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
-
-require 'fastlane' # to import the Action super class
-require 'fastlane/plugin/saucectl' # import the actual plugin
-
-Fastlane.load_actions # load other actions (in case your plugin calls other actions or shared values)
